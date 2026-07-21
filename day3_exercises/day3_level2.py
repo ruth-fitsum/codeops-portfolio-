245755343d2eae5e5c1c269c8271821d4d117220
Ruth <ruth.endeshaw.fitsum@gmail.com>
2026-07-21 09:24:01 +0300

    modify both utils and day3_level2 files

--- day3_exercises/day3_level2.py
+++ day3_exercises/day3_level2.py
@@ -12,9 +12,28 @@ print(num_List.sort())
 
 # by using only a method we can have the sum
 total=sum(num_List)
-print(total)
 
 # there is no built in method for average 
 average=total/len(num_List)
-print(average)
+
+# 5, Dictionary Operations
+
+products={"shiro":145,"tefe":200,"oil":300,"bread":15,"yogurt":100}
+for product,price in products.items():
+    print(f"The current price for {product} is {price} Birr.")
+
+user=input("Enter a product name to know the price: ")
+print(products.get(user,"Product is not available in the price book."))
+
+# 6, List Comprehension
+
+nums=[i for i in range(1,21)]
+even_nums=[i for i in range(1,31) if i%2==0 ]
+odd_nums=[i for i in range(1,11) if i%2==1]
+
+# 7, Modules & Import
+
+from utils import add_tax
+
+add_tax(20,0.2)
 
--- day3_exercises/utils.py
+++ day3_exercises/utils.py
@@ -1,4 +1,5 @@
 def add_tax(price,rate=0.15):
     tax_amount=price*rate
     price_tax=price+tax_amount
-    return price_tax
+    print(f"Your total price has become {price_tax} Birr.")
+    
